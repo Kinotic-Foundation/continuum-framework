d2d7f5df4b9795f9729b78d402b97cb6c9bbcf50
Navid Mitchell <navid@mitchellsoftware.net>
2023-05-31 23:24:31 -0600

    Small change

--- continuum-idl/src/main/java/org/kinotic/continuum/idl/api/schema/UnionC3Type.java
+++ continuum-idl/src/main/java/org/kinotic/continuum/idl/api/schema/UnionC3Type.java
@@ -21,6 +21,16 @@ import java.util.List;
 public class UnionC3Type extends C3Type {
 
     /**
+     * The namespace that this {@link UnionC3Type} belongs to
+     */
+    private String namespace = null;
+
+    /**
+     * This is the name of the {@link UnionC3Type} such as "Animal"
+     */
+    private String name = null;
+
+    /**
      * This is the field that will be used to determine which type is being used.
      * The field must exist on all types in this union.
      * The value of the field must match the name of one of the {@link ObjectC3Type}'s in types

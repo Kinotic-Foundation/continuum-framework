5f4e4fd3de856ad5ec2c1a0292fae2184cf594cc
Navid Mitchell <navid@mitchellsoftware.net>
2023-06-08 05:49:43 -0600

    Comments

--- continuum-idl/src/main/java/org/kinotic/continuum/idl/api/converter/C3ConversionContext.java
+++ continuum-idl/src/main/java/org/kinotic/continuum/idl/api/converter/C3ConversionContext.java
@@ -13,8 +13,17 @@ import org.kinotic.continuum.idl.api.schema.C3Type;
  */
 public interface C3ConversionContext<R, S> {
 
+    /**
+     * Converts the given {@link C3Type} to the type specified by the {@link C3ConversionContext}
+     *
+     * @param c3Type to convert
+     * @return the converted value
+     */
     R convert(C3Type c3Type);
 
+    /**
+     * @return the state of this {@link C3ConversionContext}
+     */
     S state();
 
 }

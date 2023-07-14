8348e9ea03d60cf9099bee0fa58568afba10a37c
Navid Mitchell <navid@mitchellsoftware.net>
2023-07-14 02:27:32 -0600

    Making sure build CLI works

--- continuum-js/continuum-cli/src/commands/create/project.ts
+++ continuum-js/continuum-cli/src/commands/create/project.ts
@@ -9,103 +9,103 @@ import upperFirst from 'lodash-es/upperFirst.js'
 import {createFrontEnd} from '../../internal/CommandHelper.js'
 
 export class Project extends Command {
-  static description = 'Creates a Continuum Project'
+    static description = 'Creates a Continuum Project'
 
-  static examples = [
-    `$ continuum create project MyContinuumProject`,
-  ]
+    static examples = [
+        `$ continuum create project MyContinuumProject`,
+    ]
 
-  static args = {
-    name: Args.string({description: 'The Name for the Project', required: true})
-  }
-
-  async run(): Promise<void> {
-    const {args} = await this.parse(Project)
-
-    let projectDir: string = path.resolve(args.name)
-
-    let context: any = { projectName: args.name}
-
-    ux.action.start('Syncing project templates')
-    await templateRepositoryManager.updateRepositoryIfNecessary()
-    ux.action.stop()
-
-    this.log('Creating Continuum Project')
-    context = await spawnEngine.renderSpawn('project', projectDir, context)
-
-    this.log('Creating Continuum Gateway')
-    process.chdir(projectDir)
-    // now create Continuum Gateway project
-    await spawnEngine.renderSpawn('project-gateway',
-                                  context.projectName + '-gateway',
-                                  {...context,
-                                           name: upperFirst(context.projectName) + 'Gateway',
-                                           artifactId: context.projectName.toLowerCase() + '-gateway',
-                                           basePackage: context.groupId + '.gateway'
-                                  }
-    )
-
-    ux.action.start('Storing Templates')
-    await spawnResolver.copySpawnsFromRepositoryToDirectory('microservice-common', 'project-microservice', 'project-library')
-    ux.action.stop()
-
-    let answers: any
-    let first: boolean = true
-    do {
-
-      answers = await inquirer.prompt([
-        {
-          type: 'list',
-          name: 'type',
-          message: 'What would you like to add?',
-          choices: ['Microservice', 'Library', 'Frontend', 'Quit']
-        },
-        {
-          type: 'input',
-          name: 'artifactId',
-          message: 'Maven Artifact Id',
-          when: (ans: any) => {
-            return  ans?.type != 'Frontend' && ans?.type != 'Quit'
-          }
-        }
-      ])
-
-      let artifactId: string = answers.artifactId
-      context.artifactId = artifactId
-
-      switch(answers.type){
-        case 'Microservice':
-          await this.renderProjectModule('project-microservice', artifactId, projectDir, context);
-          break;
-        case 'Library':
-          await this.renderProjectModule('project-library', artifactId, projectDir, context);
-          break;
-        case 'Frontend':
-          const {name} = await inquirer.prompt([
-            {
-              type: 'input',
-              name: 'name',
-              message: 'Project Name'
-            }
-          ])
-          await createFrontEnd(name)
-          break;
-        case 'Quit':
-          break;
-      }
-
-      first = false
-    } while(answers?.type != 'Quit')
-
-  }
-
-  private async renderProjectModule(spawn: string, artifactId: string, projectDir: string, context: any) {
-    let dir: string = path.resolve(artifactId)
-
-    if (!dir.startsWith(projectDir)) {
-      throw new Error(`Artifact Dir ${dir} must be within ${projectDir}`)
+    static args = {
+        name: Args.string({description: 'The Name for the Project', required: true})
     }
 
-    await spawnEngine.renderSpawn(spawn, dir, context)
-  }
+    async run(): Promise<void> {
+        const {args} = await this.parse(Project)
+
+        let projectDir: string = path.resolve(args.name)
+
+        let context: any = { projectName: args.name}
+
+        ux.action.start('Syncing project templates')
+        await templateRepositoryManager.updateRepositoryIfNecessary()
+        ux.action.stop()
+
+        this.log('Creating Continuum Project')
+        context = await spawnEngine.renderSpawn('project', projectDir, context)
+
+        this.log('Creating Continuum Gateway')
+        process.chdir(projectDir)
+        // now create Continuum Gateway project
+        await spawnEngine.renderSpawn('project-gateway',
+            context.projectName + '-gateway',
+            {...context,
+                name: upperFirst(context.projectName) + 'Gateway',
+                artifactId: context.projectName.toLowerCase() + '-gateway',
+                basePackage: context.groupId + '.gateway'
+            }
+        )
+
+        ux.action.start('Storing Templates')
+        await spawnResolver.copySpawnsFromRepositoryToDirectory('microservice-common', 'project-microservice', 'project-library')
+        ux.action.stop()
+
+        let answers: any
+        let first: boolean = true
+        do {
+
+            answers = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'type',
+                    message: 'What would you like to add?',
+                    choices: ['Microservice', 'Library', 'Frontend', 'Quit']
+                },
+                {
+                    type: 'input',
+                    name: 'artifactId',
+                    message: 'Maven Artifact Id',
+                    when: (ans: any) => {
+                        return  ans?.type != 'Frontend' && ans?.type != 'Quit'
+                    }
+                }
+            ])
+
+            let artifactId: string = answers.artifactId
+            context.artifactId = artifactId
+
+            switch(answers.type){
+                case 'Microservice':
+                    await this.renderProjectModule('project-microservice', artifactId, projectDir, context);
+                    break;
+                case 'Library':
+                    await this.renderProjectModule('project-library', artifactId, projectDir, context);
+                    break;
+                case 'Frontend':
+                    const {name} = await inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'name',
+                            message: 'Project Name'
+                        }
+                    ])
+                    await createFrontEnd(name)
+                    break;
+                case 'Quit':
+                    break;
+            }
+
+            first = false
+        } while(answers?.type != 'Quit')
+
+    }
+
+    private async renderProjectModule(spawn: string, artifactId: string, projectDir: string, context: any) {
+        let dir: string = path.resolve(artifactId)
+
+        if (!dir.startsWith(projectDir)) {
+            throw new Error(`Artifact Dir ${dir} must be within ${projectDir}`)
+        }
+
+        await spawnEngine.renderSpawn(spawn, dir, context)
+    }
 }

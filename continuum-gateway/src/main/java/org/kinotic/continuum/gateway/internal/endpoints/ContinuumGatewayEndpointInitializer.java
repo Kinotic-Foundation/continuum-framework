522e6f56045149cfc443fea8823b905c92cc6d25
Navid Mitchell <navid@mitchellsoftware.net>
2024-03-06 21:45:07 -0700

    gitflow-hotfix-stash: StartVerticlePerCore

--- continuum-gateway/src/main/java/org/kinotic/continuum/gateway/internal/endpoints/ContinuumGatewayEndpointInitializer.java
+++ continuum-gateway/src/main/java/org/kinotic/continuum/gateway/internal/endpoints/ContinuumGatewayEndpointInitializer.java
@@ -17,12 +17,14 @@
 
 package org.kinotic.continuum.gateway.internal.endpoints;
 
+import lombok.RequiredArgsConstructor;
 import org.kinotic.continuum.gateway.internal.endpoints.rest.RestServerVerticle;
 import org.kinotic.continuum.gateway.internal.endpoints.stomp.StompServerVerticle;
 import io.vertx.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -32,28 +34,25 @@ import javax.annotation.PostConstruct;
  * Created by navid on 2/10/20
  */
 @Component
+@RequiredArgsConstructor
 public class ContinuumGatewayEndpointInitializer {
     private static final Logger log = LoggerFactory.getLogger(ContinuumGatewayEndpointInitializer.class);
 
-    @Autowired
-    private Vertx vertx;
-
-    /**
-     * Autowire all endpoint below.
-     * This is done one by one instead of all Verticles since we do not want clients to have side effects..
-     */
-    @Autowired
-    private StompServerVerticle stompServerVerticle;
-
-    @Autowired
-    private RestServerVerticle restServerVerticle;
+    private final StompServerVerticle stompServerVerticle;
+    private final RestServerVerticle restServerVerticle;
+    private final Environment environment;
+    private final Vertx vertx;
 
     @PostConstruct
     public void init(){
-        log.info("Deploying Stomp Server Endpoint");
+        // If production deploy one verticle of each per core
+        int numToDeploy = Math.min(environment.matchesProfiles("production") ? Math.max(Runtime.getRuntime().availableProcessors(), 1) : 1, 8);
+        log.info(numToDeploy + " Cores will be used for Continuum Endpoints");
+
+        log.info("Deploying Stomp Server Endpoint " + numToDeploy);
         vertx.deployVerticle(stompServerVerticle);
 
-        log.info("Deploying REST Server Endpoint");
+        log.info("Deploying REST Server Endpoint " + numToDeploy);
         vertx.deployVerticle(restServerVerticle);
     }
 

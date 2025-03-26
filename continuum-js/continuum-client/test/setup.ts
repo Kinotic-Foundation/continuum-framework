f64c2d1798e5a4cb3ace0733cd1bb246d1fb0f73
Navid Mitchell <navid@mitchellsoftware.net>
2025-03-25 20:20:54 -0700

    Get continuum version from properties

--- continuum-js/continuum-client/test/setup.ts
+++ continuum-js/continuum-client/test/setup.ts
@@ -1,24 +1,39 @@
+import path from 'node:path'
 import {GenericContainer, PullPolicy, StartedTestContainer, Wait} from 'testcontainers'
 import {TestProject} from 'vitest/node.js'
+import { readFileSync } from 'node:fs'
+import { getProperties } from 'properties-file'
 
 let container: StartedTestContainer
 
 // Run once before all tests
 export async function setup(project: TestProject) {
-    console.log('Starting Continuum Gateway')
-    container = await new GenericContainer('kinotic/continuum-gateway-server:latest')
-        .withExposedPorts(58503)
-        .withEnvironment({SPRING_PROFILES_ACTIVE: "clienttest"})
-        .withPullPolicy(PullPolicy.alwaysPull())
-        .withWaitStrategy(Wait.forHttp('/', 58503))
-        .start()
-
     // @ts-ignore
-    project.provide('CONTINUUM_HOST', container.getHost())
-    // @ts-ignore
-    project.provide('CONTINUUM_PORT', container.getMappedPort(58503))
+    if(import.meta.env.VITE_USE_GATEWAY_DOCKER === 'true') {
+        console.log('Starting Continuum Gateway')
 
-    console.log('Continuum Gateway started.')
+        const props = getProperties(readFileSync(path.resolve('../../', 'gradle.properties')))
+
+        container = await new GenericContainer(`kinotic/continuum-gateway-server:${props['continuumVersion']}`)
+            .withExposedPorts(58503)
+            .withEnvironment({SPRING_PROFILES_ACTIVE: "clienttest"})
+            .withPullPolicy(PullPolicy.alwaysPull())
+            .withWaitStrategy(Wait.forHttp('/', 58503))
+            .start()
+
+        // @ts-ignore
+        project.provide('CONTINUUM_HOST', container.getHost())
+        // @ts-ignore
+        project.provide('CONTINUUM_PORT', container.getMappedPort(58503))
+
+        console.log('Continuum Gateway started.')
+    }else{
+        // @ts-ignore
+        project.provide('CONTINUUM_HOST', '127.0.0.1')
+        // @ts-ignore
+        project.provide('CONTINUUM_PORT', 58503)
+        console.log('Skipping Continuum Gateway start because VITE_USE_GATEWAY_DOCKER is false')
+    }
 }
 
 // Run once after all tests

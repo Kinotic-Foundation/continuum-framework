bfc3342e0d6ef12a567f8ebe3ab2a65d35d10711
Nicholas Padilla <nicholas@mindsignited.com>
2025-09-17 07:53:18 -0700

    fix: ensure we can use continuum-client in cjs envs

--- continuum-js/continuum-client/vite.config.ts
+++ continuum-js/continuum-client/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'continuum',
             fileName: 'continuum',
-            formats: ["es"],
+            formats: ["es", "cjs"],
         },
         sourcemap: true,
     },
